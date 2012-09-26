23453fcc8a3c0c34931f7c1b3102965749bf6e38
Pontus Lurcock <pont@talvi.net>
2012-09-26 12:09:51 -0400

    Added docstring at the start of the program.

--- clgplot.py
+++ clgplot.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+A simple interactive application for plotting IRM acquisition data.
+
+clgplot reads two types of file: raw IRM acquisition data (as a two-column
+file containing applied field and measured magnetization), and output files
+from the IrmUnmix program [1] which contain a representation of IRM
+acquisition data as a sum of cumulative log-Gaussian (CLG) curves. clgplot
+produces a plot of the data and/or curves using pylab. The plot can be viewed
+on-screen and saved to a file.
+
+clgplot is copyright 2012 by Pontus Lurcock, who may be contacted at
+pont talvi net
+    @     .
+
+[1] See http://www.geo.uu.nl/~forth/Software/irmunmix/ and
+http://dx.doi.org/10.1046/j.0956-540x.2001.01558.x
+'''
+
 import os, re, pylab
 import Tkinter as Tki
 from os.path import basename
